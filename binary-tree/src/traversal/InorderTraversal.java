03d7c92cd9b6c096aa6c737c2f391689e4bf944f
Atri Raha <atriraha@gmail.com>
2023-04-08 17:11:57 +0530

    Modifying Inorder traversal

--- binary-tree/src/traversal/InorderTraversal.java
+++ binary-tree/src/traversal/InorderTraversal.java
@@ -5,6 +5,20 @@ import node.Node;
 import java.util.Objects;
 
 public class InorderTraversal implements TreeTraversal {
+
+    /**
+     * In Inorder traversal we recursively visit left subtree,
+     * then process the root and then the right subtree.
+     *
+     * Time Complexity: O(n)
+     * Constant amount of work for every node.
+     *
+     * Auxiliary Space: O(h)
+     * Since in worst case h+1 function calls will be there in function call stack.
+     * Where h is the height of binary tree.
+     *
+     * @param rootNode
+     */
     @Override
     public void traverse(Node rootNode) {
         if (Objects.nonNull(rootNode)) {
